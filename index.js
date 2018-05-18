2b33a2f7a78de3c5033734594a55e43af0418b0a
Franklin <38558436+FrankTeck@users.noreply.github.com>
2018-05-18 19:53:58 +0200

    [Bêta] du bot LEAD

--- index.js
+++ index.js
@@ -1,108 +1,57 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-var prefix = "/"
+var prefix = "l/"
 
 bot.on('ready', function () {
     console.log("Je suis connecté !")
 })
 
 bot.on('ready', function () {
-    bot.user.setGame('coder le bot de Crazy_Street_').catch(console.error)
+    bot.user.setGame('L-Drago [l/help]-> This is a dragon').catch(console.error)
 })
 
 bot.on('guildMemberAdd', function (member) {
-    member.createDM().then(function (channel) {
-        return channel.send('Bienvenue ' + member.displayName + ' sur le serveur de Crazy_Street, je t\'invite à aller voir les règles, amuse toi bien sur le Discord :smile: ')
-    }).catch(console.error)
-})
+  member.createDM().then(function (channel) {
+    return channel.send('Bienvenue ' + member.displayName + ' sur le serveur de Lead, visite le discord, fait attention , il peut y avoir des dragons^^, amuses-toi bien sur le Discord :smile: ')
+  }).catch(console.error)
+
+  var channel = member.guild.channels.find('name', 'bienvenue_bye');
+  if (!channel) return
+  var bvn = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setTitle("Un nouveau Dragon rejoins le discord LEAD !")
+  .setDescription(`\n Bienvenue **${member.user}** sur le Discord de LEAD :smile:`)
+  .setFooter("Je t'invite à visiter le discord, et aussi d'aller voir les règles :smiley: ;)", member.user.displayAvatarURL)
+  channel.sendEmbed(bvn)
+});
+
+bot.on('guildMemberRemove', function (member) {
+  
+    var channel = member.guild.channels.find('name', 'bienvenue_bye');
+    if (!channel) return
+    var bvn = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setTitle("Un Dragon quitte le discord LEAD !")
+      .setDescription(`\n Au revoir**${member.user}**, reviens nous voir avec un meilleur dragon :smile:`)
+      .setFooter("Au revoir :smiley: ;)", member.user.displayAvatarURL)
+    channel.sendEmbed(bvn)
+    });
 
 bot.on('message', message => {
-
-    if (message.content === prefix + "help") {
-        var help_embed = new Discord.RichEmbed()
-            .setColor("#4FB448")
-            .setThumbnail("https://images-ext-1.discordapp.net/external/veZptUu_KDKmwtUJX5QT3QxESYCaRp4_k0XUwEQxubo/https/i.imgur.com/e9DIB8e.png?width=80&height=80")
-            .setTitle("Commande du bot - CrazyBot")
-            .setDescription("Liste des commandes")
-            .addField("/crazystreet", "Info sur Crazy_Street_")
-            .addField("/Unosial", "Info sur Unosial")
-            .addField("/host", "En développement")
-            .addField("/botinfo", "Info sur le bot")
-            .addField("/invite", "Lien d'invitation pour rejoindre le discord")
-        message.channel.sendMessage(help_embed);
-        console.log("Un MiniCrazy a utilisé la commande /help")
-    }
-
-    if (message.content === prefix + "help") {
-        var help_embed = new Discord.RichEmbed()
-            .setColor("#4FB448")
-            .setThumbnail("https://images-ext-1.discordapp.net/external/veZptUu_KDKmwtUJX5QT3QxESYCaRp4_k0XUwEQxubo/https/i.imgur.com/e9DIB8e.png?width=80&height=80")
-            .setTitle("Commande de modération - CrazyBot")
-            .setDescription("Liste des commandes")
-            .addField("/mute", "En développement")
-            .addField("/unmute", "En développement")
-            .addField("/ban", "En développement")
-            .addField("/kick", "En développement")
-        message.channel.sendMessage(help_embed);
-    }
-
-    if (message.content === prefix + "crazystreet") {
-        var help_embed = new Discord.RichEmbed()
-            .setColor("#4FB448")
-            .setThumbnail("https://yt3.ggpht.com/-crHFyzLCViM/AAAAAAAAAAI/AAAAAAAAAAA/SfJVEztH9HQ/s288-mo-c-c0xffffffff-rj-k-no/photo.jpg")
-            .setTitle("Crazy_Street_")
-            .setDescription("18 ans, vidéaste sur YouTube développeur pour Unosial")
-            .addField("Youtube", "https://www.youtube.com/user/CrazyCraftHC/about")
-            .addField("Twitter", "https://twitter.com/crazycrafthc_yt?lang=fr")
-        message.channel.sendMessage(help_embed);
-        console.log("Un MiniCrazy veut plus d'info sur Crazy_Street en faisant la commande /CrazyStreet")
-    }
-
-    if (message.content === prefix + "unosial") {
-        var help_embed = new Discord.RichEmbed()
-            .setColor("#4FB448")
-            .setThumbnail("https://pbs.twimg.com/profile_images/950780773178728448/XlM9XmLN_400x400.jpg")
-            .setTitle("Unosial")
-            .setDescription("Serveur Minecraft Survie 1.12.2 [Actuellement en Développement] CM: @Aeverr_ @Typholz")
-            .addField("IP du serveur", "On l'auras l'IP mais pas aujourd'hui")
-            .addField("Twitter", "https://twitter.com/crazycrafthc_yt?lang=fr")
-        message.channel.sendMessage(help_embed);
-        console.log("Un MiniCrazy veut plus d'info sur Unosial en faisant la commande /Unosial")
-    }
-
-    if (message.content === prefix + "botinfo") {
-        var help_embed = new Discord.RichEmbed()
-            .setColor("#4FB448")
-            .setThumbnail("https://minotar.net/helm/Crazy_Street_/300.png")
-            .setTitle("CrazyBot")
-            .setDescription("Info du Bot CrazyBot")
-            .addField("Nom", "@CrazyBot#8719")
-            .addField("Version", "Bêta")
-            .addField("Creator", "@Franklin#3692")
-            .addField("Crée le", "18/04/2018")
-            .addField("Pour", "Le Discord de Crazy_Street_")
-            .addField("Invite boit", "Ce bot est uniquement accesible au discord de Crazy_Street_, il est donc privée")
-        message.channel.sendMessage(help_embed);
-        console.log("Un MiniCrazy veut plus d'info sur le bot en faisant la commande /botinfo")
-    }
-
 
     if (message.content === prefix + "invite") {
         var help_embed = new Discord.RichEmbed()
             .setColor("#4FB448")
-            .setThumbnail("https://minotar.net/helm/Crazy_Street_/300.png")
+            .setThumbnail("https://minotar.net/helm/Watter_Fire_/300.png")
             .setTitle("Liens d'invitation")
             .setDescription("Voici le lien d'invitation pour inviter tes amis sur le Discord :smile:")
-            .addField("Invitation:", "https://discord.gg/2TWTSrs")
+            .addField("Invitation:", "https://discord.gg/veuQK7M")
         message.channel.sendMessage(help_embed);
-        console.log("Un MiniCrazy veut inviter un pote sur le serveur en faisant la commande /invite")
+        console.log("Un Dragon veut inviter un pote sur le serveur en faisant la commande /invite")
     }
 
 
 
-    if (message.content === "JUL") {
-        message.reply(":jul:");
-    }
 
     if (message.content === "tg") {
         message.delete()
@@ -196,4 +145,4 @@ bot.on('message', message => {
     }
 })
 
-bot.login('NDM1MzgyNzAzMzY2MzQwNjA4.DbZX1g.AEy9UUKbnit7agIeUIyCZ_sIRls')
+bot.login(process.env.TOKEN)
